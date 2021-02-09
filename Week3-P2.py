93216dec4ba3448aa51de36104f52e601759ecf2
flynnn5 <flynnn5@tcd.ie>
2021-02-09 22:56:29 +0000

    Week Three exercises 1 -3

--- Week3-P2.py
+++ Week3-P2.py
@@ -134,3 +134,37 @@ banking_imputed = banking_fullid.fillna({'acct_amount':acct_imp})
 
 # Print number of missing values
 print(banking_imputed.isna().sum())
+
+#STRINGS
+#PART ONE
+# Import process from fuzzywuzzy
+from fuzzywuzzy import process
+
+# Store the unique values of cuisine_type in unique_types
+unique_types = restaurants['cuisine_type'].unique()
+
+# Calculate similarity of 'asian' to all values of unique_types
+print(process.extract('asian', unique_types, limit = len(unique_types)))
+
+# Calculate similarity of 'american' to all values of unique_types
+print(process.extract('american', unique_types, limit = len(unique_types)))
+# Calculate similarity of 'italian' to all values of unique_types
+print(process.extract('italian', unique_types, limit = len(unique_types)))
+
+#PART TWO
+# Iterate through categories
+for cuisine in categories:
+    # Create a list of matches, comparing cuisine with the cuisine_type column
+    matches = process.extract(cuisine, restaurants['cuisine_type'], limit=len(restaurants.cuisine_type))
+
+    # Iterate through the list of matches
+    for match in matches:
+        # Check whether the similarity score is greater than or equal to 80
+        if match[1] >= 80:
+            # If it is, select all rows where the cuisine_type is spelled this way, and set them to the correct cuisine
+            restaurants.loc[restaurants['cuisine_type'] == match[0]] = cuisine
+
+# Inspect the final result
+restaurants['cuisine_type'].unique()
+
+#PART Three
