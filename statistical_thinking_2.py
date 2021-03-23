dd03a553dff7e4e67fcd57ba8fc664ff958daa2e
flynnn5 <flynnn5@tcd.ie>
2021-03-23 22:28:12 +0000

    Statistical Analysis and thinking

---
+++ statistical_thinking_2.py
@@ -0,0 +1,289 @@
+# Seed random number generator
+np.random.seed(42)
+
+# Compute mean no-hitter time: tau
+tau = np.mean(nohitter_times)
+
+# Draw out of an exponential distribution with parameter tau: inter_nohitter_time
+inter_nohitter_time = np.random.exponential(tau, 100000)
+
+# Plot the PDF and label axes
+_ = plt.hist(inter_nohitter_time,
+             bins=50, normed=True, histtype='step')
+_ = plt.xlabel('Games between no-hitters')
+_ = plt.ylabel('PDF')
+
+# Show the plot
+plt.show()
+
+#ECDF AND CDF
+# Create an ECDF from real data: x, y
+x, y = ecdf(nohitter_times)
+
+# Create a CDF from theoretical samples: x_theor, y_theor
+x_theor, y_theor = ecdf(inter_nohitter_time)
+
+# Overlay the plots
+plt.plot(x_theor, y_theor)
+plt.plot(x, y, marker='.', linestyle='none')
+
+# Margins and axis labels
+plt.margins(0.02)
+plt.xlabel('Games between no-hitters')
+plt.ylabel('CDF')
+
+# Show the plot
+plt.show()
+
+#PART THREE - EXPONENTIAL
+# Plot the theoretical CDFs
+plt.plot(x_theor, y_theor)
+plt.plot(x, y, marker='.', linestyle='none')
+plt.margins(0.02)
+plt.xlabel('Games between no-hitters')
+plt.ylabel('CDF')
+
+# Take samples with half tau: samples_half
+samples_half = np.random.exponential(tau/2, size=10000)
+
+# Take samples with double tau: samples_double
+samples_double = np.random.exponential(tau*2, size=10000)
+
+# Generate CDFs from these samples
+x_half, y_half = ecdf(samples_half)
+x_double, y_double = ecdf(samples_double)
+
+# Plot these CDFs as lines
+_ = plt.plot(x_half, y_half)
+_ = plt.plot(x_double, y_double)
+
+# Show the plot
+plt.show()
+
+#LINEAR REGRESSION
+# Plot the illiteracy rate versus fertility
+_ = plt.plot(illiteracy, fertility, marker='.', linestyle='none')
+
+# Set the margins and label axes
+plt.margins(0.02)
+_ = plt.xlabel('percent illiterate')
+_ = plt.ylabel('fertility')
+
+# Show the plot
+plt.show()
+
+# Show the Pearson correlation coefficient
+print(pearson_r(illiteracy, fertility))
+
+#LINEAR REGRESSION - PLOT THE LINE
+# Plot the illiteracy rate versus fertility
+_ = plt.plot(illiteracy, fertility, marker='.', linestyle='none')
+plt.margins(0.02)
+_ = plt.xlabel('percent illiterate')
+_ = plt.ylabel('fertility')
+
+# Perform a linear regression using np.polyfit(): a, b
+a, b = np.polyfit(illiteracy, fertility, 1)
+
+# Print the results to the screen
+print('slope =', a, 'children per woman / percent illiterate')
+print('intercept =', b, 'children per woman')
+
+# Make theoretical line to plot
+x = np.array([0,100])
+y = a * x + b
+
+# Add regression line to your plot
+_ = plt.plot(x, y)
+
+# Draw the plot
+plt.show()
+
+#GET RESIDUAL SUM OF SQUARES
+# Specify slopes to consider: a_vals
+a_vals = np.linspace(0, 0.1, num=200)
+
+# Initialize sum of square of residuals: rss
+rss = np.empty_like(a_vals)
+
+# Compute sum of square of residuals for each value of a_vals
+for i, a in enumerate(a_vals):
+    rss[i] = np.sum((fertility - a*illiteracy - b)**2)
+
+# Plot the RSS
+plt.plot(a_vals, rss, '-')
+plt.xlabel('slope (children per woman / percent illiterate)')
+plt.ylabel('sum of square of residuals')
+
+plt.show()
+
+#LINE OF BEST FIT
+# Perform linear regression: a, b
+a, b = np.polyfit(x,y, 1)
+
+# Print the slope and intercept
+print(a, b)
+
+# Generate theoretical x and y data: x_theor, y_theor
+x_theor = np.array([3, 15])
+y_theor = a * x_theor + b
+
+# Plot the Anscombe data and theoretical line
+_ = plt.plot(x, y, marker='.', linestyle='none')
+_ = plt.plot(x_theor, y_theor)
+
+# Label the axes
+plt.xlabel('x')
+plt.ylabel('y')
+
+# Show the plot
+plt.show()
+
+#FOR LOOP FOR SLOPE AND INTERCEPT
+# Iterate through x,y pairs
+for x, y in zip(anscombe_x , anscombe_y ):
+    # Compute the slope and intercept: a, b
+    a, b = np.polyfit(x, y, 1)
+
+    # Print the result
+    print('slope:', a, 'intercept:', b)
+
+#BOOSTRAPPING
+for i in range(50):
+    # Generate bootstrap sample: bs_sample
+    bs_sample = np.random.choice(rainfall, size=len(rainfall))
+
+    # Compute and plot ECDF from bootstrap sample
+    x, y = ecdf(bs_sample)
+    _ = plt.plot(x, y, marker='.', linestyle='none',
+                 color='gray', alpha=0.1)
+
+# Compute and plot ECDF from original data
+x, y = ecdf(rainfall)
+_ = plt.plot(x, y, marker='.')
+
+# Make margins and label axes
+plt.margins(0.02)
+_ = plt.xlabel('yearly rainfall (mm)')
+_ = plt.ylabel('ECDF')
+
+# Show the plot
+plt.show()
+
+#BOOTSTRAP SAMPLES FOR LOOP FOR BOOTSTRAP REPLICAS
+def draw_bs_reps(data, func, size=1):
+    """Draw bootstrap replicates."""
+
+    # Initialize array of replicates: bs_replicates
+    bs_replicates = np.empty(size)
+
+    # Generate replicates
+    for i in range(size):
+        bs_replicates[i] = bootstrap_replicate_1d(data, func)
+
+    return bs_replicates
+
+#BOOSTRAP REPILCATES OF MEAN AND SEM
+# Take 10,000 bootstrap replicates of the mean: bs_replicates
+bs_replicates = draw_bs_reps(rainfall, np.mean, 10000)
+
+# Compute and print SEM
+sem = np.std(rainfall) / np.sqrt(len(rainfall))
+print(sem)
+
+# Compute and print standard deviation of bootstrap replicates
+bs_std = np.std(bs_replicates)
+print(bs_std)
+
+# Make a histogram of the results
+_ = plt.hist(bs_replicates, bins=50, normed=True)
+_ = plt.xlabel('mean annual rainfall (mm)')
+_ = plt.ylabel('PDF')
+
+# Show the plot
+plt.show()
+
+#COMPUTE BOOSTRAP FOR VARIANCE
+# Generate 10,000 bootstrap replicates of the variance: bs_replicates
+bs_replicates = draw_bs_reps(rainfall, np.var, 10000)
+
+# Put the variance in units of square centimeters
+bs_replicates = bs_replicates/100
+
+# Make a histogram of the results
+_ = plt.hist(bs_replicates, normed=True, bins=50)
+_ = plt.xlabel('variance of annual rainfall (sq. cm)')
+_ = plt.ylabel('PDF')
+
+# Show the plot
+plt.show()
+
+
+#ADDING CONFIDENCE INTERVALS FOR ANALYSIS
+# Draw bootstrap replicates of the mean no-hitter time (equal to tau): bs_replicates
+bs_replicates = draw_bs_reps(nohitter_times, np.mean, 10000)
+
+# Compute the 95% confidence interval: conf_int
+conf_int = np.percentile(bs_replicates, [2.5, 97.5])
+
+# Print the confidence interval
+print('95% confidence interval =', conf_int, 'games')
+
+# Plot the histogram of the replicates
+_ = plt.hist(bs_replicates, bins=50, normed=True)
+_ = plt.xlabel(r'$\tau$ (games)')
+_ = plt.ylabel('PDF')
+
+# Show the plot
+plt.show()
+
+#PAIRS BOOTSTRAP
+def draw_bs_pairs_linreg(x, y, size=1):
+    """Perform pairs bootstrap for linear regression."""
+
+    # Set up array of indices to sample from: inds
+    inds = np.arange(0, len(x))
+
+    # Initialize replicates: bs_slope_reps, bs_intercept_reps
+    bs_slope_reps = np.empty(size)
+    bs_intercept_reps = np.empty(size)
+
+    # Generate replicates
+    for i in range(size):
+        bs_inds = np.random.choice(inds, size=len(inds))
+        bs_x, bs_y = x[bs_inds], y[bs_inds]
+        bs_slope_reps[i], bs_intercept_reps[i] = np.polyfit(bs_x, bs_y, 1)
+
+    return bs_slope_reps, bs_intercept_reps
+
+#PAIRS BOOTSRAP CONTINUED
+# Generate replicates of slope and intercept using pairs bootstrap
+bs_slope_reps, bs_intercept_reps = draw_bs_pairs_linreg(illiteracy, fertility, 1000)
+
+# Compute and print 95% CI for slope
+print(np.percentile(bs_slope_reps, [2.5, 97.5]))
+
+# Plot the histogram
+_ = plt.hist(bs_slope_reps, bins=50, normed=True)
+_ = plt.xlabel('slope')
+_ = plt.ylabel('PDF')
+plt.show()
+
+#PLOTTING BOOTSTRAP PAIRS
+# Generate array of x-values for bootstrap lines: x
+x = np.array([0,100])
+
+# Plot the bootstrap lines
+for i in range(100):
+    _ = plt.plot(x,
+                 bs_slope_reps[i]*x + bs_intercept_reps[i],
+                 linewidth=0.5, alpha=0.2, color='red')
+
+# Plot the data
+_ = plt.plot(illiteracy, fertility, marker='.', linestyle='none')
+
+# Label axes, set the margins, and show the plot
+_ = plt.xlabel('illiteracy')
+_ = plt.ylabel('fertility')
+plt.margins(0.02)
+plt.show()
